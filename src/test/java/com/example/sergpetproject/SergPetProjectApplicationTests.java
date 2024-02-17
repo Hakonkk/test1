5faf6777ec4e7ca24f6b264cec7031d7e6127270
Popov Aleksander <Aleksandr.A.Popov@rosbank.ru>
2024-02-18 02:53:27 +0600

    first commit

---
+++ src/test/java/com/example/sergpetproject/SergPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sergpetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SergPetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
